c0d3d71dcd9f305156d3ed71e0fbadbe0c2c3102
littlewhywhat <littlewhywhat@gmail.com>
2016-12-31 03:58:50 +0100

    added thymeleaf: create form for question, index and show views of questions; configured autoreloading of server

--- src/main/java/com/sss/controller/QuestionsController.java
+++ src/main/java/com/sss/controller/QuestionsController.java
@@ -5,36 +5,62 @@ import com.sss.model.bo.QuestionBO;
 import com.sss.model.vo.QuestionVO;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
-@RestController
+@Controller
 class QuestionsController {
 
+	private static final String QUESTION = "question"; 
+	private static final String QUESTIONS = QUESTION + "s";
+	private static final String INDEX = QUESTIONS + "/index";
+	private static final String QUESTION_ID = QUESTIONS + "/{" + QUESTION + "Id}";
+	private static final String SHOW = QUESTIONS + "/show";
+	private static final String NEW = QUESTIONS + "/new"; 
+	
     @Autowired
-    IQuestionsService questions;
+    private IQuestionsService questions;
 
-    @RequestMapping("/questions")
-    public List<QuestionBO> index() {
-        return questions.all();
+    @ModelAttribute(QUESTIONS)
+    public List<QuestionBO> all() {
+    	return questions.all();
     }
     
-    @RequestMapping(method=RequestMethod.POST, value="/questions")
-    public void add(@RequestBody QuestionVO questionVO) {    	
-    	questions.add(questionVO);
+    @RequestMapping(QUESTIONS)
+    public String index() {
+        return INDEX;
     }
 
-    @RequestMapping(method=RequestMethod.PUT, value="questions/{questionId}")
+    @RequestMapping(NEW)
+    public String create(Model model) {
+    	model.addAttribute(QUESTION, new QuestionVO());
+    	return NEW;
+    }
+    
+    @RequestMapping(method=RequestMethod.POST, value=QUESTIONS)
+    public String add(@ModelAttribute QuestionVO questionVO) {    	
+    	questions.add(questionVO);
+    	return INDEX;
+    }
+
+    @RequestMapping(QUESTION_ID)
+    public String show(@PathVariable Long questionId, Model model) {
+    	model.addAttribute(QUESTION, questions.find(questionId));
+    	return SHOW;
+    }
+    
+    @RequestMapping(method=RequestMethod.PUT, value=QUESTION_ID)
     public void update(@RequestBody QuestionVO questionVO, @PathVariable Long questionId) {
     	questions.update(questionVO, questionId);
     }
     
-    @RequestMapping(method=RequestMethod.DELETE, value="questions/{questionId}")
+    @RequestMapping(method=RequestMethod.DELETE, value=QUESTION_ID)
     public void delete(@PathVariable Long questionId) {
     	questions.delete(questionId);
     }
