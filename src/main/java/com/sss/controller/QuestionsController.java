3a052da4ffa2313fd502029962ebe010d460f495
littlewhywhat <littlewhywhat@gmail.com>
2017-01-09 03:23:26 +0100

    removed unused imports from QuestionsController

--- src/main/java/com/sss/controller/QuestionsController.java
+++ src/main/java/com/sss/controller/QuestionsController.java
@@ -5,8 +5,6 @@ import com.sss.model.bo.QuestionBO;
 import com.sss.model.vo.QuestionVO;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
